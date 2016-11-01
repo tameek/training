cd038a2a9b2e4c54e5b8940975385429436b4cd9
tameek <tameek@gmail.com>
2016-11-01 11:47:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,3 +23,22 @@ provider "aws" {
   secret_key = "VfOebGuAesa/jcDK+fgvpUYkwRXKUUea42HIcsIQ"
   region     = "us-east-1"
 }
+resource "aws_instance" "datapipe-dog"{
+  ami           = "ami-0d729a60"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-57e40c2a"]
+  subnet_id = "subnet-d56fbc8e"
+
+tags {
+        Identity = "datapipe-dog"
+        Name = "thenderson"
+        Env = "lab"
+    }
+}
+
+#output "State" {
+#    value = "${instance_state}"
+#}
+#module "test_module" {
+#source = "example-module"
+#}
