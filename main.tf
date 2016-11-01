807cd66134c7a2aab61279efd2ecafdb2bf535e5
tameek <tameek@gmail.com>
2016-11-01 15:32:58 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your subnet ID is:
+#
+#     subnet-d56fbc8e
+#
+# Your security group ID is:
+#
+#     sg-57e40c2a
+#
+# Your AMI ID is:
+#
+#     ami-c9580bde
+#
+# Your Identity is:
+#
+#     datapipe-silkworm
+#
+
+provider "aws" {
+  access_key = "AKIAJJTO5BVZCLHRG3RA"
+  secret_key = "VfOebGuAesa/jcDK+fgvpUYkwRXKUUea42HIcsIQ"
+  region     = "us-east-1"
+}
